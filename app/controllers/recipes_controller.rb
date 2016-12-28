bd78b79bf0c2f2765d5f47c2d7e8062f00a0ef51
michaelwishlow <michael@pantheritconsulting.com>
2016-12-28 12:13:23 -0500

    Views/Edit/Destroy

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,6 +3,7 @@ class RecipesController < ApplicationController
 
 
 		def index
+			@recipe = Recipe.all.order("created_at DESC")
 
 		end
 
@@ -20,11 +21,29 @@ class RecipesController < ApplicationController
 			@recipe = Recipe.new(recipe_params)
 
 			if @recipe.save
-					redirect_to @recipe, notice: "Successfuly created new recipe"
+					redirect_to @recipe, notice: "Successfully created new recipe"
 			else
 				render 'new'
 			end
 		end
+
+		def edit
+		end
+
+		def update
+			if @recipe.update(recipe_params)
+				redirect_to @recipe
+			else
+				render "edit"
+			end
+		end
+
+		def destroy
+			@recipe.destroy
+			redirect_to root_path, notice: "Succesfully deleted Recipe"
+  end
+
+
 
 		private
 
